a73bd2579f927bb84344457eb1c1712b609ab3b3
farizzulfiannur <farizzulfiannur@gmail.com>
2024-06-10 09:45:29 +0700

    Develop add todo feature

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const TodoForm = ({ addTodo }) => {
+
+  const [title, setTitle] =  useState('');
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(title);
+    setTitle('')
+  }
+
+  return (
+    <div style={styles.container}>
+        <form onSubmit={(event) => {
+            handleSubmit(event);
+        }}
+        >
+            <input type="text" placeholder='Add your Todo' style={styles.formInput} onChange={(event) => handleChangeTitle(event)} value={title}/>
+            <button style={styles.button}> Add Todo </button>
+        </form>
+    </div>
+  )
+}
+
+const styles = {
+    container: {
+      marginBottom: '32px',
+    },
+    formInput: {
+      height: '66px',
+      width: '40%',
+      fontSize: '16px',
+      padding: '0 16px',
+    },
+    button: {
+      height: '72px',
+      fontSize: '16px',
+    },
+  }
+  
+
+export default TodoForm
